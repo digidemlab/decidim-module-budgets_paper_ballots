f032dbfee512ff8521f1cddf1bc204d64a0c4d99
Pierre Mesure <pierre@mesu.re>
2022-06-04 15:56:30 +0200

    Include/prepend overrides and import in Budgets component

---
+++ app/cells/decidim/budgets_paper_ballots/project_votes_count_cell_override.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Decidim
+  module BudgetsPaperBallots
+    module ProjectVotesCountCellOverride
+      def content
+        if options[:layout] == :one_line
+          safe_join([model.total_votes, " ", label(t("decidim.budgets.projects.project.votes",
+                                                                count: model.total_votes))])
+        else
+          safe_join([number, label(t("decidim.budgets.projects.project.votes",
+                                     count: model.total_votes))])
+        end
+      end
+
+      def number
+        content_tag :div, model.total_votes, class: "text-large"
+      end
+    end
+  end
+end
--- lib/decidim/budgets_paper_ballots/engine.rb
+++ lib/decidim/budgets_paper_ballots/engine.rb
@@ -3,12 +3,35 @@
 require "rails"
 require "decidim/core"
 require "decidim/budgets"
+require "decidim/budgets_paper_ballots/import"
+require "decidim/budgets_paper_ballots/project_serializer"
 
 module Decidim
   module BudgetsPaperBallots
     # This is the engine that runs on the public interface of budgets_paper_ballots.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::BudgetsPaperBallots
+
+      config.to_prepare do
+        Decidim::Budgets::ProjectVotesCountCell.prepend(ProjectVotesCountCellOverride)
+        Decidim::Budgets::Project.include(ProjectOverride)
+        Decidim::Budgets::ProjectSerializer.prepend(ProjectSerializerOverride)
+      end
+
+      initializer "budgets_paper_ballots.component_override" do
+        Decidim.component_registry.find(:budgets).tap do |component|
+          component.imports :paper_ballot_result do |imports|
+            imports.messages do |msg|
+              msg.set(:resource_name) { |count: 1| I18n.t("decidim.budgets.admin.imports.resources.paper_ballot_results", count: count) }
+              msg.set(:title) { I18n.t("decidim.budgets.admin.imports.title.paper_ballot_results") }
+              msg.set(:label) { I18n.t("decidim.budgets.admin.imports.label.paper_ballot_results") }
+              msg.set(:help) { I18n.t("decidim.budgets.admin.imports.help.paper_ballot_results") }
+            end
+
+            imports.creator Decidim::BudgetsPaperBallots::Import::PaperBallotResultCreator
+          end
+        end
+      end
     end
   end
 end
