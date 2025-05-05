038fce005325495698b2b8918183561526384c68
Rupus Reinefjord <rupus@digidemlab.org>
2025-05-05 12:03:54 +0000

    fix vote count on public budget page

--- app/cells/decidim/budgets_paper_ballots/project_votes_count_cell_override.rb
+++ app/cells/decidim/budgets_paper_ballots/project_votes_count_cell_override.rb
@@ -5,16 +5,18 @@ module Decidim
     module ProjectVotesCountCellOverride
       def content
         if options[:layout] == :one_line
-          safe_join([model.total_votes, " ", label(t("decidim.budgets.projects.project.votes",
-                                                     count: model.total_votes))])
+          safe_join([model.total_votes, " ", count_label])
         else
-          safe_join([number, label(t("decidim.budgets.projects.project.votes",
-                                     count: model.total_votes))])
+          safe_join([number, count_label])
         end
       end
 
       def number
         content_tag :div, model.total_votes, class: "text-large"
+      end
+
+      def count_label
+        content_tag(:span, t("decidim.budgets.projects.project.votes", count: model.total_votes))
       end
     end
   end
