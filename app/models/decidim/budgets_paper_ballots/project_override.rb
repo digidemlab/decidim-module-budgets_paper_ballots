2892df1d7415f37c17d84ad1e40f637ffae2ccb2
Rupus Reinefjord <rupus@digidemlab.org>
2025-05-09 14:29:24 +0000

    sort votes by total

--- app/models/decidim/budgets_paper_ballots/project_override.rb
+++ app/models/decidim/budgets_paper_ballots/project_override.rb
@@ -10,6 +10,28 @@ module Decidim
                  foreign_key: "decidim_project_id",
                  class_name: "Decidim::BudgetsPaperBallots::PaperBallotResult",
                  dependent: :destroy
+
+        ransacker :total_votes do
+          query = <<-SQL.squish
+          (
+            SELECT SUM(votes) as total_votes
+            FROM
+              (
+                SELECT COUNT(decidim_budgets_line_items.decidim_order_id) as votes
+                FROM decidim_budgets_line_items
+                LEFT JOIN decidim_budgets_orders
+                ON decidim_budgets_orders.id = decidim_budgets_line_items.decidim_order_id
+                WHERE decidim_budgets_orders.checked_out_at IS NOT NULL
+                AND decidim_budgets_line_items.decidim_project_id = decidim_budgets_projects.id
+                UNION ALL
+                SELECT votes
+                FROM decidim_budgets_paper_ballot_results as ballots
+                WHERE ballots.decidim_project_id = decidim_budgets_projects.id
+              )
+          )
+          SQL
+          Arel.sql(query)
+        end
       end
 
       # Public: Returns the number of paper votes for a specific project
